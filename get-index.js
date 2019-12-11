5b1babb7e08bd9b87f0611ba3c31069e0590f91a
András Jáky <jakyandras@gmail.com>
2019-12-11 12:51:11 +0100

    separated function and test files

--- get-index.js
+++ get-index.js
@@ -1,8 +1,6 @@
 'use strict';
 
-let test = require('tape');
-
-function getIndex (list, value) {
+export function getIndex (list, value) {
     let index = [];
 
     for (let i= 0; i < list.length; i++) {
@@ -19,36 +17,3 @@ function getIndex (list, value) {
         return index;
     }
 }
-
-test('one match', t => {
-    let list = [2, 5, 8, 4, 6, 3, 9];
-    let value = 4;
-
-    let actual = getIndex(list, value);
-    let expected = 3;
-
-    t.equal(actual, expected);
-    t.end();
-});
-
-test('zero match', t => {
-    let list = [2, 5, 8, 4, 6, 3, 9];
-    let value = 7;
-
-    let actual = getIndex(list, value);
-    let expected = -1;
-
-    t.equal(actual, expected);
-    t.end();
-});
-
-test('multiple matches', function (t) {
-    let list = [2, 5, 8, 4, 6, 3, 9, 6];
-    let value = 6;
-
-    let actual = getIndex(list, value);
-    let expected = [4, 7];
-
-    t.deepEqual(actual, expected);
-    t.end();
-});
---
+++ tests.js
@@ -0,0 +1,38 @@
+'use strict';
+
+import { getIndex } from './get-index';
+
+let test = require('tape');
+
+test('one match', t => {
+    let list = [2, 5, 8, 4, 6, 3, 9];
+    let value = 4;
+
+    let actual = getIndex(list, value);
+    let expected = 3;
+
+    t.equal(actual, expected);
+    t.end();
+});
+
+test('zero match', t => {
+    let list = [2, 5, 8, 4, 6, 3, 9];
+    let value = 7;
+
+    let actual = getIndex(list, value);
+    let expected = -1;
+
+    t.equal(actual, expected);
+    t.end();
+});
+
+test('multiple matches', function (t) {
+    let list = [2, 5, 8, 4, 6, 3, 9, 6];
+    let value = 6;
+
+    let actual = getIndex(list, value);
+    let expected = [4, 7];
+
+    t.deepEqual(actual, expected);
+    t.end();
+});
